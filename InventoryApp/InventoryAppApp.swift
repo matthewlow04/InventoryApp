fab131745c3c40d5746a51199e8dd18576a98320
Matthew Low <matthew7419@gmail.com>
2023-09-13 13:51:47 -0400

    Initial Commit

---
+++ InventoryApp/InventoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryAppApp.swift
+//  InventoryApp
+//
+//  Created by Matthew Low on 2023-09-13.
+//
+
+import SwiftUI
+
+@main
+struct InventoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

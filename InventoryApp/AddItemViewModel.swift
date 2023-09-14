e6a2fa8eab10780babb292998309e12b933daeea
Matthew Low <matthew7419@gmail.com>
2023-09-14 13:46:58 -0400

    Added item view model

---
+++ InventoryApp/AddItemViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  AddItemViewModel.swift
+//  InventoryApp
+//
+//  Created by Matthew Low on 2023-09-13.
+//
+
+import Foundation
+
+class AddItemViewModel: ObservableObject{
+    
+    @Published var name = ""
+    @Published var notes = ""
+    @Published var showingAlert = false
+    @Published var alertMessage = "Item added"
+    
+    func clearFields(){
+        name = ""
+        notes = ""
+    }
+    
+}

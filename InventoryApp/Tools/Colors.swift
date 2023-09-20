c4b94edad823617981a683cbaafd89cc70d49f4e
Matthew Low <matthew7419@gmail.com>
2023-09-20 12:43:14 -0400

    Added color theme and login screen background

---
+++ InventoryApp/Tools/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  InventoryApp
+//
+//  Created by Matthew Low on 2023-09-20.
+//
+
+import SwiftUI
+
+public class CustomColor{
+    static let textBlue = Color("TextBlue")
+    static let skyBlue = Color("SkyBlue")
+    static let lightBlue = Color("LightBlue")
+    static let aquamarine = Color("Aquamarine")
+}
---
+++ InventoryApp/Views/BackgroundView.swift
@@ -0,0 +1,48 @@
+//
+//  BackgroundView.swift
+//  InventoryApp
+//
+//  Created by Matthew Low on 2023-09-20.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        
+        VStack{
+            Image("generis-logo-color")
+            VStack(alignment: .leading, spacing: 50){
+                
+                Spacer()
+                    .frame(maxWidth: .infinity)
+                
+               
+              
+                    
+                
+                Rectangle()
+                    .foregroundColor(CustomColor.lightBlue)
+                    .frame(width: 100, height: 25)
+                    .cornerRadius(20)
+                Rectangle()
+                    .foregroundColor(CustomColor.skyBlue)
+                    .frame(width: 150, height: 25)
+                    .cornerRadius(20)
+                Rectangle()
+                    .foregroundColor(CustomColor.aquamarine)
+                    .frame(width: 200, height: 25)
+                    .cornerRadius(20)
+                    
+          
+            }.opacity(0.6)
+        }
+       
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView()
+    }
+}

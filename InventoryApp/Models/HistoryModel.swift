7a35ce4af3fb6e07c9b8326a4bdcdb093e2e82eb
Matthew Low <matthew7419@gmail.com>
2023-09-25 15:17:58 -0400

    Added history model

---
+++ InventoryApp/Models/HistoryModel.swift
@@ -0,0 +1,22 @@
+//
+//  HistoryModel.swift
+//  InventoryApp
+//
+//  Created by Matthew Low on 2023-09-25.
+//
+
+import Foundation
+
+struct History: Hashable, Identifiable{
+    var id = UUID()
+    var itemName: String
+    var date: Date
+    var addedItem: Bool
+    var amount: Int
+    
+    func addedItemString(_ addedItem: Bool) -> String{
+        return addedItem ? "+":"-"
+    }
+}
+
+
